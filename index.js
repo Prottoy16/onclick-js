bc267ff2f513ca5773d5d9b1282cdaab0cb55ebd
Prottoy16 <prottoybiswas16@gmail.com>
2023-11-27 20:02:54 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var paragraph=document.querySelector("p");
+
+
+
+
+function add(){
+   
+    paragraph.classList.add("style-one");
+
+}
+
+function remove(){
+    paragraph.classList.remove("style-one");
+}
